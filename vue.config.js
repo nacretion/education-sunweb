b802d974074a01e1e3b4155483669b3a062a2c08
mirml <nacretion@gmail.com>
2023-11-26 18:08:42 +0500

    add svg-loader and vuex lib

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: "src/app.js",
+      title: "Евробетон",
+    },
+  },
+  chainWebpack: (config) => {
+    config.module.rules.delete("svg");
+  },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.svg$/,
+          loader: "vue-svg-loader",
+        },
+      ],
+    },
+  },
 });
