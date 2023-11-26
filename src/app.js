b21c3b00d8c8160ab71adc0266c51be5bfb37012
mirml <nacretion@gmail.com>
2023-11-26 19:05:20 +0500

    использование сторы

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import isMobileStore from "@/stores/isMobileStore";
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
+  store: isMobileStore,
 }).$mount("#app");
